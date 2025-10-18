2fd4a9def4d74b2ab7ea95728a3e14b62a95238c
d-chernov <d-chernov@alkona.it>
2025-10-18 03:38:03 +0300

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import tailwindcss from '@tailwindcss/vite';
+const ASSET_URL = process.env.ASSET_URL || '';
 // https://vite.dev/config/
 export default defineConfig({
+  base: `${ASSET_URL}/dist/`,
   plugins: [vue(),tailwindcss(),],
 })
