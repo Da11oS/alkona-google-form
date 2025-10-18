839f7cdbccce2fb9e0a105ea42d80b5f9b44aa42
d-chernov <d-chernov@alkona.it>
2025-10-18 03:44:08 +0300

    empty base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 const ASSET_URL = process.env.ASSET_URL || '';
 // https://vite.dev/config/
 export default defineConfig({
-  base: `${ASSET_URL}/dist/`,
+  base: '',
   plugins: [vue(),tailwindcss(),],
 })
