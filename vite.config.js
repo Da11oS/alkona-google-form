09721d2af93252849d831c9c1fdef50a6c76660d
d-chernov <d-chernov@alkona.it>
2025-10-18 03:48:58 +0300

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 const ASSET_URL = process.env.ASSET_URL || '';
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://da11os.github.io/alkona-google-form/',
+  base: process.env.NODE_ENV === 'production' ? '/alkona-google-form/actions/' : '/',
   plugins: [vue(),tailwindcss(),],
 })
